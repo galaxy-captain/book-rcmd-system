880ffa6770b3d2575cc77275f4ee8ab26d155c73
Xiaolei Duan <xiaolei.duan@shopee.com>
2020-08-24 19:25:11 +0800

    init

--- book-rcmd-golang/data_source.go
+++ book-rcmd-golang/data_source.go
@@ -1,1 +1,22 @@
-package book_rcmd_golang
+package rcmd
+
+import "sync"
+
+type DataSource interface {
+	Read(count int) []interface{}
+}
+
+type MySqlDataSource struct {
+	lock *sync.Mutex
+}
+
+func (m *MySqlDataSource) init() {
+	m.lock = new(sync.Mutex)
+}
+
+func (m *MySqlDataSource) Read(count int) (result []interface{}) {
+
+	result = make([]interface{}, 0, count)
+
+	return
+}
