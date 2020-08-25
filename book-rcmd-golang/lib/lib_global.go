21ccd1004356b6b1477d6c63da947853d631a51e
galaxy-captain <xiaolei.duan@shopee.com>
2020-08-25 16:35:46 +0800

    init

--- book-rcmd-golang/lib/lib_global.go
+++
@@ -1,7 +0,0 @@
-package lib
-
-import "fmt"
-
-func init() {
-	fmt.Println("lib init")
-}
--- book-rcmd-golang/lib/safe_goroutine.go
+++ book-rcmd-golang/lib/safe_goroutine.go
@@ -1,1 +1,14 @@
 package lib
+
+import "fmt"
+
+func Go(f func()) {
+	go func(f func()) {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("Error - a panic happened: %v\n", r)
+			}
+		}()
+		f() // execute the real-function
+	}(f)
+}
